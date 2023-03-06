dd106b3ed8bec4f8df4d206b93699d55f458db2e
Alon Landmann <rafael.landmann@gmail.com>
2023-03-06 19:54:37 +0100

    [add] link components

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,42 @@
 import Navbar from './Navbar'
+import css from '@/css/Home.module.css'
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+
+      <div className={css.mainSection}>
+        <div className={css.links}>
+          <div className={css.link}>
+            <div className={css.image}><i className='bi bi-book'></i></div>
+            <div className={css.name}>Linear Algebra Done Right</div>
+            <button>edit</button>
+            <button>delete</button>
+            <div><i className='bi bi-bookmark'></i></div>
+            <div><i className='bi bi-arrow-right'></i></div>
+            <div><i className='bi bi-bookmark-check-fill'></i></div>
+          </div>
+          <div className={css.link}>
+            <div className={css.image}><i className='bi bi-book'></i></div>
+            <div className={css.name}>Linear Algebra Done Right</div>
+            <button>edit</button>
+            <button>delete</button>
+            <div><i className='bi bi-bookmark'></i></div>
+            <div><i className='bi bi-arrow-right'></i></div>
+            <div><i className='bi bi-bookmark-check-fill'></i></div>
+          </div>
+          <div className={css.link}>
+            <div className={css.image}><i className='bi bi-book'></i></div>
+            <div className={css.name}>Linear Algebra Done Right</div>
+            <button>edit</button>
+            <button>delete</button>
+            <div><i className='bi bi-bookmark'></i></div>
+            <div><i className='bi bi-arrow-right'></i></div>
+            <div><i className='bi bi-bookmark-check-fill'></i></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
