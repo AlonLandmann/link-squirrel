9026fc0a57b6ddc7a1174475765f707fdbfdf895
Alon Landmann <rafael.landmann@gmail.com>
2023-03-06 19:26:16 +0100

    [add] navbar

--- components/Home.js
+++ components/Home.js
@@ -1,18 +1,9 @@
-import { useUser } from '@auth0/nextjs-auth0/client'
+import Navbar from './Navbar'
 
 export default function Home() {
-  const { user, error, isLoading } = useUser()
-
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div>{error.message}</div>
-
   return (
     <div>
-      <div>Hello, {user ? user.name : 'dear visitor'}!</div>
-      {user
-        ? <div><a href='/api/auth/logout'>Log out</a></div>
-        : <div><a href='/api/auth/login'>Log in</a></div>
-      }
+      <Navbar />
     </div>
   )
 }
---
+++ components/Navbar.js
@@ -0,0 +1,25 @@
+import { useUser } from '@auth0/nextjs-auth0/client'
+import css from '@/css/Navbar.module.css'
+
+export default function Navbar() {
+  const { user, error, isLoading } = useUser()
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+
+  return (
+    <div className={css.container}>
+      <div className={css.center}>
+        <div>link squirrel</div>
+        <div className={css.navLinks}>
+          <div>About</div>
+          <div>Contact</div>
+          {user
+            ? <div><a href='/api/auth/logout'>Log out</a></div>
+            : <div><a href='/api/auth/login'>Log in</a></div>
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
