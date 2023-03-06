e25a1879d52ac83d7011c7aaea94a0d57627c655
Alon Landmann <rafael.landmann@gmail.com>
2023-03-06 17:45:36 +0100

    [add] home page component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Head from 'next/head'
+import Home from '@/components/Home'
 
 export default function Index() {
   return (
     <>
       <Head>
         <title>Link Squirrel</title>
+        <link rel='icon' href='/favicon.svg' />
         <meta name='description' content='collect and organize educational resources' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.svg' />
       </Head>
 
       <main>
-        Hello world!
+        <Home />
       </main>
     </>
   )
