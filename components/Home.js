68669eb25db462df76f615bd9dfbce5e271ecd9c
Alon Landmann <rafael.landmann@gmail.com>
2023-03-12 17:10:25 +0100

    [add] placeholder for no saved links

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,19 @@
 import Navbar from './Navbar'
+import css from '@/scss/Home.module.scss'
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+
+      <div>
+        <div className={css.center}>
+          <h1>Looks like you haven't saved any links yet.</h1>
+          <h3>Press the button to add a new link!</h3>
+          <i className='bi bi-link-45deg'></i>
+          <button>add new link</button>
+        </div>
+      </div>
     </div>
   )
 }
