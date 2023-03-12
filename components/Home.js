4977ab99feba46803b86b06591ae80ffb04157be
Alon Landmann <rafael.landmann@gmail.com>
2023-03-12 18:14:18 +0100

    [change] order of elements on page

--- components/Home.js
+++ components/Home.js
@@ -12,9 +12,9 @@ export default function Home() {
 
       <div>
         <div className={css.center}>
+          <i className='bi bi-link-45deg'></i>
           <h1>Looks like you haven't saved any links yet.</h1>
           <h3>Press the button below to add a new link!</h3>
-          <i className='bi bi-link-45deg'></i>
           <button onClick = {() => { setFormInView(true) }}>
             add new link
           </button>
