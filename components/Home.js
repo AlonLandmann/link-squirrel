33a91eee52eecc627d8a9b2b080fc3aec1741c60
Alon Landmann <rafael.landmann@gmail.com>
2023-03-12 18:04:28 +0100

    [add] link form skeleton

--- components/Home.js
+++ components/Home.js
@@ -14,6 +14,39 @@ export default function Home() {
           <button>add new link</button>
         </div>
       </div>
+
+      <div className={css.popUpFormContainer}>
+        <div className={css.popUpForm}>
+          <h2>Link Form</h2>
+          <div className={css.escapeFormButton}>
+            <i className='bi bi-x-lg soft-link'></i>
+          </div>
+          <form>
+            <input
+              type='text'
+              placeholder='url'
+            />
+            <input
+              type='text'
+              placeholder='title'
+            />
+            <select>
+              <option value={null} selected>- type -</option>
+              <option value='video'>video</option>
+              <option value='text'>text</option>
+              <option value='course'>course</option>
+              <option value='other'>other</option>
+            </select>
+            <select>
+              <option value={null} selected>- status -</option>
+              <option value='bookmarked'>bookmarked</option>
+              <option value='in focus'>in focus</option>
+              <option value='completed'>completed</option>
+            </select>
+            <button>add link</button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
