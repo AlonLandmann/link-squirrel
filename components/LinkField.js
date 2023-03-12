b07aefdbae7a71f110069577ecb2a85491304645
Alon Landmann <rafael.landmann@gmail.com>
2023-03-12 22:57:09 +0100

    [add] link icon on hover

--- components/LinkField.js
+++ components/LinkField.js
@@ -3,11 +3,21 @@ import css from '@/scss/LinkField.module.scss'
 
 export default function LinkField({ title, href, type, initialStatus }) {
   const [status, setStatus] = useState(initialStatus)
+  const [linkIconInView, setLinkIconInView] = useState(false)
 
   return (
     <div className={css.link}>
-      <div className={css.type}><i className={`bi bi-${type}`}></i></div>
-      <div className={css.title}>{title}</div>
+      <div
+        onMouseEnter={() => { setLinkIconInView(true) }}
+        onMouseLeave={() => { setLinkIconInView(false) }}
+        onClick={() => { window.open(href, '_blank').focus() }}
+      >
+      {linkIconInView &&
+        <div className={css.linkIcon}><i className='bi bi-box-arrow-up-right'></i></div>
+      }
+        <div className={css.type}><i className={`bi bi-${type}`}></i></div>
+        <div className={css.title}>{title}</div>
+      </div>
       <div className={css.status}>
         <div
           className={status === 'saved' && css.selected}
