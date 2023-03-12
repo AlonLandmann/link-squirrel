26804e60500e3fab5264b5bb32853bf89393e41b
Alon Landmann <rafael.landmann@gmail.com>
2023-03-12 17:11:49 +0100

    [change] text instructions

--- components/Home.js
+++ components/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
       <div>
         <div className={css.center}>
           <h1>Looks like you haven't saved any links yet.</h1>
-          <h3>Press the button to add a new link!</h3>
+          <h3>Press the button below to add a new link!</h3>
           <i className='bi bi-link-45deg'></i>
           <button>add new link</button>
         </div>
