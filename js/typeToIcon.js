fbda4579e76e943607e5058c2e9358af75154bc2
Alon Landmann <rafael.landmann@gmail.com>
2023-03-13 17:34:21 +0100

    [add] icon for other types

--- js/typeToIcon.js
+++ js/typeToIcon.js
@@ -2,4 +2,5 @@ export default function typeToIcon(type) {
   if (type === 'video') return 'bi bi-collection-play-fill'
   if (type === 'text') return 'bi bi-book'
   if (type === 'course') return 'bi bi-laptop'
+  if (type === 'other') return 'bi bi-box'
 }
